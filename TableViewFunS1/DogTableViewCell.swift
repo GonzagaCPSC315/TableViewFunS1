39d7e39f72480c2c00f8ad20ab07d8a1aaad393d
Gina Sprint <sprint@gonzaga.edu>
2020-10-23 10:17:31 -0700

    wire up labels

---
+++ TableViewFunS1/DogTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DogTableViewCell.swift
+//  TableViewFunS1
+//
+//  Created by Gina Sprint on 10/23/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class DogTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var breedLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
